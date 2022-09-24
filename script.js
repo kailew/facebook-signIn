9159b5d84d1d110ec5afb119b21913d4c81ef756
Kai Lew <kailew2020@gmail.com>
2022-09-23 18:42:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var todos = [
+	"clean room",
+	"brush teeth",
+	"exercise",
+	"study javascript",
+	"eat healthy"
+];
+var todosLength = todos.length;
+for (var i=0; i < todos.length; i++) {
+	todos.pop();
+}
